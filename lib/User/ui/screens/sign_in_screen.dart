4bb14a1b9cd1caadca10b5b2cfce9f450d8d6e0f
iblurdesigner <nevomen@gmail.com>
2020-04-28 16:22:44 -0500

    Boton generico verde

---
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../../../widgets/gradient_back.dart';
+import 'package:platzitripsapp/widgets/button_green.dart';
+
+class SignInScreen extends StatefulWidget {
+
+  @override
+  State createState() {
+    return _SignInScreen();
+  }
+}
+
+
+class _SignInScreen extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return signInGoogleUI();
+  }
+
+  Widget signInGoogleUI() {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          GradientBack("", null),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                  "Welcome \n This is your Travel App.",
+                style: TextStyle(
+                  fontSize: 37.0,
+                  fontFamily: "Lato",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              
+              ButtonGreen(
+                text: "Login with Gmail",
+                onPressed: (){},
+                width: 300.0,
+                height: 50.0,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
